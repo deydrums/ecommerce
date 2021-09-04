1099f1bef579a207d51e5b4b95260d93927c84ec
deydrums <dagarcia100@gmail.com>
2021-09-03 22:57:25 -0600

    Registro

--- backend/controllers/ClientController.js
+++ backend/controllers/ClientController.js
@@ -2,13 +2,41 @@
 
 const {response} = require('express');
 const Client = require('../models/Client');
-
+var bcrypt = require('bcryptjs');
 
 const registerClient = async(req,res = response)=>{
-    res.status(200).json({
-        ok: true,
-        message: 'Hola'
-    })
+    const {email, password} = req.body;
+    try {
+        let client = await Client.findOne({email: email});
+        if(client){
+            return res.status(400).json({
+                ok: false,
+                message: 'El correo ya fue registrado anteriormente'
+            });
+        };
+        client = new Client(req.body);
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        client.password = bcrypt.hashSync(password,salt);
+
+        await client.save();
+        //Generar nuestro JWT
+        //const token = await generateJWT(user.id, user.name);
+
+    
+        res.status(201).json({
+            ok: true,
+            message: 'Registro exitoso',
+            client
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Ha ocurrido un error, intenta de nuevo'
+        })
+    }
 }
 module.exports = {
     registerClient,
