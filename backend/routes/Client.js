89c2f58e961e07e986b387c850f55d990b9e8cea
deydrums <dagarcia100@gmail.com>
2021-09-03 23:03:22 -0600

    params validator

--- backend/routes/Client.js
+++ backend/routes/Client.js
@@ -6,11 +6,20 @@
  * */ 
 
  const {Router} = require('express');
+ const { check } = require('express-validator');
  const router = Router();
  const { registerClient } = require('../controllers/ClientController');
- 
+ const { paramsValidator } = require('../middlewares/params-validator');
+
  router.post(
         '/register',
+        [
+            check('name', 'El nombre no es valido').not().isEmpty(),
+            check('surname', 'El apellido no es valido').not().isEmpty(),
+            check('email', 'El email no es valido').isEmail(),
+            check('password', 'El password debe de ser de 8 caracteres').isLength({min: 8}), 
+            paramsValidator
+        ],
         registerClient
     );
 
