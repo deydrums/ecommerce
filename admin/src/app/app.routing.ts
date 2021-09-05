43e974808f822272aa6c04716404119e7bbf6af0
deydrums <dagarcia100@gmail.com>
2021-09-05 10:42:09 -0600

    Rutas

---
+++ admin/src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { IndexComponent } from "./components/index/index.component";
+
+const appRoute: Routes = [
+    {path: '', component: IndexComponent},
+];
+
+export const appRoutingProviders: any[] =[];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
