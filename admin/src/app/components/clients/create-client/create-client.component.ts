c7bc5a168dea83f5a7872c8a6da90ed76e05da35
deydrums <dagarcia100@gmail.com>
2021-09-13 20:41:11 -0600

    Formulario de crear cliente

--- admin/src/app/components/clients/create-client/create-client.component.ts
+++ admin/src/app/components/clients/create-client/create-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var iziToast:any;
 
 @Component({
   selector: 'app-create-client',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClientComponent implements OnInit {
 
+  public client : any = {};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  register(registerForm : any){
+    if(registerForm.valid){
+      console.log(this.client);
+    }else{
+      iziToast.show({
+        title: 'Error',
+        titleColor: '#ff0000',
+        color: '#fff',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Los datos del formulario no son validos'
+      })
+    }
   }
 
 }
