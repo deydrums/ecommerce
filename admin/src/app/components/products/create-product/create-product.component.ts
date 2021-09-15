3588146347917ad3445f3fecef24ea1cd3bb0bec
deydrums <dagarcia100@gmail.com>
2021-09-14 20:19:02 -0600

    Previsualizar imagen

--- admin/src/app/components/products/create-product/create-product.component.ts
+++ admin/src/app/components/products/create-product/create-product.component.ts
@@ -14,6 +14,9 @@ export class CreateProductComponent implements OnInit {
 
   }
 
+  public file : any | File = undefined;
+  public imgSelect : any | ArrayBuffer = 'assets/img/default.jpg';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -21,7 +24,8 @@ export class CreateProductComponent implements OnInit {
 
   create(createForm : any){
     if(createForm.valid){
-
+      console.log(this.product);
+      
     }else{
       iziToast.show({
         title: 'Error',
@@ -32,6 +36,54 @@ export class CreateProductComponent implements OnInit {
         message: 'Los datos del formulario no son validos'
       });
     }
+  }
+
+  fileChangeEvent(event:any) : void{
+    var file : any;
+    if(event.target.files && event.target.files[0]){
+      file = <File>event.target.files[0];
+    }else{
+      iziToast.show({
+        title: 'Error',
+        titleColor: '#ff0000',
+        color: '#fff',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'No hay una imagen valida'
+      });
+    }
+
+    if(file?.size <= 4000000){
+      if(file?.type == 'image/png' || file?.type == 'image/webp' || file?.type == 'image/jpg' || file?.type == 'image/jpeg'){
+        const reader = new FileReader();
+        reader.onload = e => this.imgSelect = reader.result;
+        reader.readAsDataURL(file);
+        this.file = file;
+      }else{
+        iziToast.show({
+          title: 'Error',
+          titleColor: '#ff0000',
+          color: '#fff',
+          class: 'text-danger',
+          position: 'topRight',
+          message: 'El formato debe de ser jpg, webp, jpg o jpeg'
+        });
+        this.imgSelect = 'assets/img/default.jpg';
+        this.file = undefined;
+      }
+    }else{
+      iziToast.show({
+        title: 'Error',
+        titleColor: '#ff0000',
+        color: '#fff',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'La imagen no puede ser mayor a 4mb'
+      });
+      this.imgSelect = 'assets/img/default.jpg';
+      this.file = undefined;
+    }
+    
   }
 
 }
