5fd831e4ddf6f2ff105b8a7f3613af6e9828948d
deydrums <dagarcia100@gmail.com>
2021-09-24 14:08:12 -0600

    Store

--- store/README.md
+++ store/README.md
@@ -1,6 +1,6 @@
 # Store
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.6.
 
 ## Development server
 
--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { routing } from './app.routing';
-import { IndexComponent } from './components/index/index.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexComponent,
-    NavComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    routing
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
