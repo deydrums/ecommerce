1305a4eeb478f3011a5a44e1edca9874f26858f5
deydrums <dagarcia100@gmail.com>
2021-09-24 14:30:12 -0600

    routin

---
+++ store/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+
+
+const appRoute: Routes = [
+    {path: '', redirectTo: 'index', pathMatch: 'full'},
+    {path: 'index', component: IndexComponent},
+];
+
+export const appRoutingProviders: any[] =[];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
