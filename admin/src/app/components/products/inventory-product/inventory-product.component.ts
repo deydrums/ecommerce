f8082e60bbbe22f4b71cafa98b81e143120cb8ee
deydrums <dagarcia100@gmail.com>
2021-09-24 13:54:32 -0600

    Exportar a excel

--- admin/src/app/components/products/inventory-product/inventory-product.component.ts
+++ admin/src/app/components/products/inventory-product/inventory-product.component.ts
@@ -5,6 +5,8 @@ import { global } from 'src/app/services/global';
 import { IziToastService } from 'src/app/services/helpers/izi-toast.service';
 import { ProductService } from 'src/app/services/product.service';
 import Swal from 'sweetalert2';
+import {Workbook} from 'exceljs';
+import * as fs  from 'file-saver'
 
 @Component({
   selector: 'app-inventory-product',
@@ -24,6 +26,8 @@ export class InventoryProductComponent implements OnInit {
   public id;
   public inventory: any = null;
   public newInventory: any = {};
+  public arr_inventory: Array<any> = [];
+
 
   constructor(
     private _productService: ProductService,
@@ -56,6 +60,14 @@ export class InventoryProductComponent implements OnInit {
             this._productService.getInventoryAdmin(this.product._id, this.token).subscribe(
               response => {
                 this.inventory = response.data;
+                this.inventory.forEach((inv: { admin: any; amount: any; supplier: any; }) =>{
+                  this.arr_inventory.push({
+                    admin: inv.admin.name + ' ' + inv.admin.surname,
+                    cantidad: inv.amount,
+                    proveedor: inv.supplier,
+                  })
+                })
+                console.log(this.arr_inventory)
                 this.loading = false;
               },
               error => {
@@ -133,5 +145,37 @@ export class InventoryProductComponent implements OnInit {
       this.loading_btn = false;
     }
   }
+
+
+
+  downloadExcel(){
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet("Reporte de productos");
+
+    worksheet.addRow(undefined);
+    for (let x1 of this.arr_inventory){
+      let x2=Object.keys(x1);
+
+      let temp=[]
+      for(let y of x2){
+        temp.push(x1[y])
+      }
+      worksheet.addRow(temp)
+    }
+
+    let fname='REP01- ';
+
+    worksheet.columns = [
+      { header: 'Admin', key: 'col1', width: 30},
+      { header: 'Cantidad', key: 'col2', width: 15},
+      { header: 'Proveedor', key: 'col3', width: 15},
+    ]as any;
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, fname+'-'+new Date().valueOf()+'.xlsx');
+    });
+  }
+
 }
 
