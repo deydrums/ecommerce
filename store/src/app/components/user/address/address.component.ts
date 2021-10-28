15efac0e7ccbc48511941a6e678fc32449a8ee25
deydrums <dagarcia100@gmail.com>
2021-10-28 12:05:17 -0600

    Address

--- store/src/app/components/user/address/address.component.ts
+++ store/src/app/components/user/address/address.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressComponent implements OnInit {
 
-  constructor() { }
+  public loading_btn:boolean;
+  public loader: boolean;
+
+  constructor() { 
+    this.loading_btn = false;
+    this.loader = true;
+  }
 
   ngOnInit(): void {
   }
